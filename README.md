12e98f804c2c7d26ac815fe69a73d6758d5c7046
Juan Leon <jleon98@toromail.csudh.edu>
2017-12-05 14:21:20 -0800

    Log in

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# toro-net-testing
+# Toro Net
+
+## Getting started with VirtualBox
+Fork the repo then clone it to your local machine.
+```
+git clone https://github.com/<your username>/toro-net.git
+```
+
+Change your directory into the dir on your VirtualBox Ubuntu virtual machine.  
+```
+cd toro-net
+```
+
+Create .env file. Add a `.env` file with your PORT, MONGO_URI, 
+[GITHUB_ID, GITHUB_SECRET and APP_URL](https://github.com/jaredhanson/passport-github) like this:
+
+***** For Frontend Team only *****
+
+*****Please refer to .env(gmofeng's github auth info); here is the link:  https://github.com/gmofeng/toro-net/blob/master/New_ENV(gmofeng's%20github%20auth%20info) *****
+
+
+```
+PORT=3000
+MONGO_URI=mongodb://127.0.0.1:27017/toro-net
+GITHUB_ID=233245n234566kjh243f
+GITHUB_SECRET=k2hj432kjgjkh2g34kj2g4jkh23g4jk2342jhg34
+APP_URL=http://127.0.0.1:3000/
+```
+
+Install dependencies (mongo, node, npm packages).  
+```
+bash dev-env.sh
+```
+
+Install node packages with npm.  
+```
+npm install
+``` 
+
+Build and watch for changes.  
+```
+npm run serve
+```
+
+Open a new terminal window and navigate to toro-net.  
+Run mongo as a service.  
+```
+sudo service mongod start
+```
+
+Run the backend using nodemon  
+```
+./node_modules/nodemon/bin/nodemon.js server.js
+```
+
+Open http://localhost:3000 in any web browser.
---
+++ src/components/Register.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="container">
+      <form action="/users" method="post" >
+      <div class="form-group">
+        <input type="text" class="form-control" name="displayName" placeholder="Display name" v-model="displayName">
+      </div>
+      <div class="form-group">
+        <input type="text" class="form-control" name="username" placeholder="Username" v-model="username">
+      </div>
+      <div class="form-group">
+        <!-- <label      class="control-label" for="email">Email</label> -->
+       <input type="text" v-validate="email" data-rules="required|email" class="form-control" name="email" placeholder="Email" v-model="email">
+
+      </div>
+      <div class="form-group">
+        <input type="password" class="form-control" name="password" placeholder="Password" v-model="password">
+      </div>
+      <div class="form-group">
+        <input type="password" class="form-control" name="passwordConf" placeholder="Confirm password" v-model="passwordConf">
+      </div>
+      <button class="btn btn-success" type="submit" >Register</button>
+      </form>
+    </div>
+
+</template>
+
+<style>
+.container form{
+
+    witdth:200px;
+
+
+
+}
+
+
+
+</style>
+
+<script>
+export default {
+  name: 'Register',
+  data() {
+    return {
+      displayName: '',
+      username: '',
+      email: '',
+      password: '',
+      passwordConf: ''
+    }
+  },
+  methods: {
+    registerUser() {
+      const newUser = {
+        displayName: this.displayName,
+        username: this.username,
+        email: this.email,
+        password: this.password,
+        passwordConf: this.passwordConf,
+      }
+      this.$store.dispatch('register', newUser)
+    }
+  }
+}
+</script>
